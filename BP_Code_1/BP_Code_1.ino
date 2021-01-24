7135162be085b59a0f9af67743510043e423eeda
Brody Wink <bwink1997@yahoo.com>
2021-01-24 16:30:37 -0500

    Code now readds data and converts to pressure

--- BP_Code_1/BP_Code_1.ino
+++ BP_Code_1/BP_Code_1.ino
@@ -3,6 +3,17 @@
 //Array to be sent to pressure sensor
 int Cmd[3]={0xAA, 0x00, 0x00};
 
+//Global Varibles
+int data[]={0,0,0};
+uint8_t data1=0;
+int count = 0;
+long int datasft1=0;
+long int datasft2=0;
+long int datasft3=0;
+long int datacomb=0;
+long int datacombfnl=0;
+long int Pressure = 0;
+
 void setup() {
  //I2C setup
   Wire.begin();        // join i2c bus (address optional for master)
@@ -29,7 +40,7 @@ void loop() {
 //  delay (10);
 //  digitalWrite(6,LOW);
 //  delay (5000);
- 
+  
 //Reading pressure sensor  
   Wire.beginTransmission(0x18); 
                               
@@ -41,11 +52,46 @@ void loop() {
   delay (10);
   
   Wire.requestFrom(0x18, 4);    // request 4 bytes from slave device 0x18
-
-  while (Wire.available()) { // slave may send less than requested
-    uint8_t c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
+  
+  while (Wire.available()) { 
+    data1 = Wire.read();
+    data[count]=data1;
+    count=count+1;
+    delay(10);
   }
-  Serial.println();
+  //Serial.print("status=");         
+  //Serial.println(data[0]);// First set of data is actually the status byte
+  //Serial.println(data[1]);
+  //Serial.println(data[2]);
+  //Serial.println(data[3]);
+  //Serial.println(data[4]);// Noise?
+  //Serial.println(count);
+
+  //Shifting data and OR-ing them
+  datasft1 = data[1];
+  datasft1 = datasft1<<16; 
+  datasft2 = data[2];
+  datasft2 = datasft2<<8;
+  datasft3 = data[3];// Doesn't need to be shifted but replacing variable for consistancy
+
+  datacomb = datasft1 | datasft2;
+  datacombfnl = datacomb | datasft3;
+
+  //Serial.println(datacombfnl);
+
+  // Calculating the actual pressure based off data
+  long int Output_Max = 15099494;
+  long int Output_Min = 1677722;
+  int P_Max = 300;
+  int P_Min = 0;
+  
+  //Function from spec
+  Pressure = (((datacombfnl-Output_Min)*(P_Max-P_Min))/(Output_Max-Output_Min))+P_Min;
+
+  Serial.println(Pressure);
+  
+
+
   delay(1000);
+  count=0;
 }
